648f8b764866e8db39447faff97a687a83d61677
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-01 00:58:24 +0530

    ADDED: implemented IGreetingService

---
+++ src/main/java/com/bridgelabz/mygreetingapp/service/GreetingService.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.mygreetingapp.service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.mygreetingapp.model.Greeting;
+import com.bridgelabz.mygreetingapp.model.User;
+
+@Service
+public class GreetingService implements IGreetingService {
+	private static final String template = "Hello, %s";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private GreetingRepository greetingRepository;
+
+	@Override
+	public Greeting addGreeting(User user) {
+		String message = String.format(template, (user.toString().isEmpty()) ? "Hello world !" : user.toString());
+		return greetingRepository.save(new Greeting(counter.incrementAndGet(), message));
+	}
+
+	@Override
+	public Greeting getGreetingById(long id) {
+			return null;
+		}
+
+	}
+
+}
