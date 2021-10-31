3aa926b842bb1403e02f172e8bd425beb59427df
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-01 00:25:14 +0530

    ADDED: created class Greeting

---
+++ src/main/java/com/bridgelabz/mygreetingapp/model/Greeting.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.mygreetingapp.model;
+
+public class Greeting {
+	private long id;
+	private String name;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
