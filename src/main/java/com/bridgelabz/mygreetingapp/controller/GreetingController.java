e1b7447c83daa4b9b6d143d3ee0ad96a6c393a73
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-01 00:47:40 +0530

    MODIFIED: greetingController to include GreetingService

--- src/main/java/com/bridgelabz/mygreetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/mygreetingapp/controller/GreetingController.java
@@ -2,21 +2,26 @@ package com.bridgelabz.mygreetingapp.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.mygreetingapp.model.Greeting;
 
 @RestController
+@RequestMapping("/greetings")
 public class GreetingController {
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 	
-	 @GetMapping("/greeting")
-	    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World")String name){
-	        return new Greeting(counter.incrementAndGet(),
-	        		String.format(template,name));
+	@Autowired
+	private IGreetingService greetingService;
+	
+	 @GetMapping("")
+	  public Greeting greeting(@RequestParam(value = "name",defaultValue = "World")String name){
+	        User user - new User();
+	        user.setFirstName(name)l
+	        return greetingService.addGreeting(user);
 	 }
 	
 
