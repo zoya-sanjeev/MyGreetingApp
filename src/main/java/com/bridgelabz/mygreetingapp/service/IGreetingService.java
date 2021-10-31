4a5f5ba0dd1eb17e6585ca3f31e250d56c927454
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-01 00:53:25 +0530

    ADDED: created interface IGreetingservice

--- src/main/java/com/bridgelabz/mygreetingapp/service/IGreetingService.java
+++ src/main/java/com/bridgelabz/mygreetingapp/service/IGreetingService.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.mygreetingapp.service;
 
+import com.bridgelabz.mygreetingapp.model.Greeting;
+import com.bridgelabz.mygreetingapp.model.User;
+
 public interface IGreetingService {
+	Greeting addGreeting(User user);
+	Greeting getGreetingById(long id);
 
 }
