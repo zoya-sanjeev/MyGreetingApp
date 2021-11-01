1f50cc43efeebf8dc534ba6bd6eb3427167edac3
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-01 23:52:09 +0530

    ADDED: greeting messages for different kinds of inputs

--- src/main/java/com/bridgelabz/mygreetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/mygreetingapp/controller/GreetingController.java
@@ -4,6 +4,10 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +30,26 @@ public class GreetingController {
 	        User user = new User();
 	        user.setFirstName(name);
 	        return greetingService.addGreeting(user);
+	 }
+	 
+	 @RequestMapping(value = { "/query" })
+	 public Greeting sayHi(@RequestParam(value = "fName", defaultValue = "Harry") String fname,
+	 		@RequestParam(value = "lName", defaultValue = "Potter") String lname) {
+	 	return new Greeting(counter.incrementAndGet(),String.format(template, fname+" "+lname));
+	 }
+
+	 @GetMapping("/param/{message}")
+	 public Greeting parameterName(@PathVariable String message) {
+	 	return new Greeting(counter.incrementAndGet(),String.format(template, message));
+	 	}
+
+	 @PostMapping("/post")
+	 public Greeting setUser(@RequestBody Greeting greeting) {
+	 	return new Greeting(counter.incrementAndGet(),String.format(template, greeting.getMessage()));
+	 }
+	 @PutMapping("/put/{firstName}")
+	 public Greeting sayHelloPutMethod(@PathVariable String firstName,@RequestParam(value="lastName",defaultValue="Potter") String lastName) {
+	 	return new Greeting(counter.incrementAndGet(),String.format(template, firstName+" "+lastName));
 	 }
 	
 
