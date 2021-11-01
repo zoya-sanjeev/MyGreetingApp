9837542b489144b067ffae23cc394df9a9b7c0cd
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-01 19:32:18 +0530

    MODIFIED: greeting class

--- src/main/java/com/bridgelabz/mygreetingapp/model/Greeting.java
+++ src/main/java/com/bridgelabz/mygreetingapp/model/Greeting.java
@@ -1,13 +1,42 @@
 package com.bridgelabz.mygreetingapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "GREETINGS")
 public class Greeting {
+	@Id
 	private long id;
 	private String message;
+	
+	public Greeting() {
+		id =0;
+		message="";
+	}
 	
 	public Greeting(long id, String message) {
 		this.id = id;
 		this.message = message;
 	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
 		
 
 }
